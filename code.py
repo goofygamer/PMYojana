589666df1c407204ce946d6519880e5a512415f7
Sankalp Yadav <51927159+goofygamer@users.noreply.github.com>
2024-04-29 22:36:32 -0500

    Update code.py
    Added subsidy_size

--- code.py
+++ code.py
@@ -10,16 +10,16 @@ class PMYojana():
         self.YOJANA_LENGTH = 25 #How long is the Yojana
         self.UNITS = 4500
 
-    def loan_amount(self, project_size: float, DCR_status: bool, loan_size: float):
+    def loan_amount(self, project_size: float, DCR_status: bool, loan_size: float, subsidy_size:float):
         '''
         If the loan_size is a percent, this function simply converts it to an amount.
         For example, if you type in loan_size = 70, this will calculate 70% of the project cost.
         '''
         if loan_size <= 100:
             if DCR_status:
-                return self.DCR * project_size * (loan_size/100)
+                return ((self.DCR * project_size) - subsidy_size) * (loan_size/100)
             else:
-                return self.NON_DCR * project_size * (loan_size/100)
+                return ((self.NON_DCR * project_size) - subsidy_size) * (loan_size/100)
         else:
             return loan_size
 
@@ -35,14 +35,14 @@ class PMYojana():
         return absolute_structure
 
 
-    def emi_payment(self, project_size: float, loan_size: float, pay_emi_in: int, bank_loan_rate = 0.105, DCR_status=True):
+    def emi_payment(self, project_size: float, loan_size: float, pay_emi_in: int, subsidy_size: float, bank_loan_rate = 0.105, DCR_status=True):
         '''
         EMI Payment structure for the specified length.
         '''
         structure = [0.0] * self.YOJANA_LENGTH
 
         #loan in percent
-        loan_size = self.loan_amount(project_size=project_size, DCR_status=DCR_status, loan_size=loan_size)
+        loan_size = self.loan_amount(project_size=project_size, DCR_status=DCR_status, loan_size=loan_size, subsidy_size=subsidy_size)
 
         emi_amount = (loan_size * bank_loan_rate)/(1 - (1 + bank_loan_rate)**(-1*pay_emi_in))
 
@@ -55,12 +55,12 @@ class PMYojana():
 
         return emi_structure
     
-    def nominal_amount(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, DCR_status=True):
+    def nominal_amount(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, subsidy_size:float, DCR_status=True):
         '''
         Nominal amount = Total amount - EMI Payment
         '''
         total_amount = self.total_amount(bid_rate=bid_rate, project_size=project_size)
-        emi_pay = self.emi_payment(project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, DCR_status=DCR_status)
+        emi_pay = self.emi_payment(project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, DCR_status=DCR_status)
 
         nominal_amount = total_amount
         nominal_amount['Nominal Amount'] = total_amount['Gross Return'] - emi_pay['EMI']
@@ -80,12 +80,12 @@ class PMYojana():
 
         return inflation_by_year
     
-    def real_amount(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, DCR_status=True):
+    def real_amount(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, subsidy_size: float, DCR_status=True):
         '''
         Nominal amount realized by the inflation factor.
         '''
         #get Nominal Amounts
-        nominal_amount = self.nominal_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, DCR_status=DCR_status)
+        nominal_amount = self.nominal_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, DCR_status=DCR_status)
         
         #turn it into Real Amounts
         real_amount = nominal_amount
@@ -97,21 +97,21 @@ class PMYojana():
 
         return real_amount[['Year', 'Real Amount']]
     
-    def annualized_return(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, realized=True, DCR_status=True):
+    def annualized_return(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, subsidy_size: float, realized=True, DCR_status=True):
         if realized:
-            amount = self.real_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in)
+            amount = self.real_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, subsidy_size=subsidy_size, pay_emi_in=pay_emi_in)
             overall_val = sum(amount['Real Amount'])
         else:
-            amount = self.nominal_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in)
+            amount = self.nominal_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, subsidy_size=subsidy_size, pay_emi_in=pay_emi_in)
             overall_val = sum(amount['Nominal Amount'])
         if DCR_status:
-            overall_investment = (project_size * self.DCR) - self.loan_amount(project_size=project_size, DCR_status=DCR_status, loan_size=loan_size)
+            overall_investment = (project_size * self.DCR) - self.loan_amount(project_size=project_size, subsidy_size=subsidy_size, DCR_status=DCR_status, loan_size=loan_size)
             if realized:
                 print(f'{round((((overall_val/overall_investment)**(1/25))-1)*100, 2)}% return over {self.INFLATION*100}% inflation over {self.YOJANA_LENGTH} years.')
             else:
                 print(f'{round((((overall_val/overall_investment)**(1/25))-1)*100, 2)}% return over {self.YOJANA_LENGTH} years.')
         else:
-            overall_investment = (project_size * self.NON_DCR) - self.loan_amount(project_size=project_size, DCR_status=DCR_status, loan_size=loan_size)
+            overall_investment = (project_size * self.NON_DCR) - self.loan_amount(project_size=project_size, subsidy_size=subsidy_size, DCR_status=DCR_status, loan_size=loan_size)
             if realized:
                 print(f'{round((((overall_val/overall_investment)**(1/25))-1)*100, 2)}% return over {self.INFLATION*100}% inflation over {self.YOJANA_LENGTH} years.')
             else:
@@ -120,13 +120,13 @@ class PMYojana():
         
         return 
 
-    def figure(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int):
+    def figure(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, subsidy_size: float):
         '''
         Draw everything.
         '''
-        emi_amount = self.emi_payment(project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in)
-        nominal_amount = self.nominal_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in)
-        real_amount = self.real_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in)
+        emi_amount = self.emi_payment(project_size=project_size, loan_size=loan_size, subsidy_size=subsidy_size, pay_emi_in=pay_emi_in)
+        nominal_amount = self.nominal_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, subsidy_size=subsidy_size, pay_emi_in=pay_emi_in)
+        real_amount = self.real_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, subsidy_size=subsidy_size, pay_emi_in=pay_emi_in)
 
         plt.plot(emi_amount['Year'], emi_amount['EMI']/1e5)
         plt.plot(emi_amount['Year'], nominal_amount['Nominal Amount']/1e5)
