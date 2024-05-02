cf8990e9038c6e1dc6ed555706b1f6b2ac90364c
Sankalp Yadav <51927159+goofygamer@users.noreply.github.com>
2024-05-01 22:19:23 -0500

    Somebody cooked here...

--- code.py
+++ code.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class PMYojana():
-    #TODO: Add a figure feature that predicts for different values of a particular variable keeping others constant. 
     def __init__(self) -> None:
         self.DCR = 33e6 #DCR Program cost per MW
         self.NON_DCR = 26e6 #Non-DCR Program cost per MW
@@ -16,7 +15,7 @@ class PMYojana():
         If the loan_size is a percent, this function simply converts it to an amount.
         For example, if you type in loan_size = 70, this will calculate 70% of the project cost.
         '''
-        if loan_size <= 100:
+        if loan_size <= 100000:
             if DCR_status:
                 return ((self.DCR * project_size - subsidy_size)) * (loan_size/100)
             else:
@@ -29,8 +28,15 @@ class PMYojana():
         '''
         The absolute return without any cost involved. Calculates the gross number.
         '''
-        nominal_per_year = project_size * 4500 * 365 * bid_rate #Return in Rupees/per year      
-        dictionary = {'Year': np.arange(1, 26), 'Gross Return': [nominal_per_year] * self.YOJANA_LENGTH}
+        nominal_per_year = project_size * 4500 * 365 * bid_rate #Return in Rupees/per year
+        total_return = [nominal_per_year] * self.YOJANA_LENGTH
+        solar_breakdown = [1.0]*25
+        solar_breakdown[0] = 0.98
+        for i in range(1, len(solar_breakdown)):
+            solar_breakdown[i] *= solar_breakdown[i-1]*0.994
+        for j in range(len(total_return)):
+            total_return[j] *= solar_breakdown[j]
+        dictionary = {'Year': np.arange(1, 26), 'Gross Return': total_return}
         absolute_structure = pd.DataFrame.from_dict(dictionary)       
 
         return absolute_structure
@@ -147,4 +153,127 @@ class PMYojana():
         plt.grid(True)
 
         self.annualized_return(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, realized=realized, DCR_status=DCR_status)
+        return
+    
+    def _annualized_return(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, subsidy_size: float, realized: bool, DCR_status: bool):
+        if realized:
+            amount = self.real_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, subsidy_size=subsidy_size, pay_emi_in=pay_emi_in)
+            overall_val = sum(amount['Real Amount'])
+        else:
+            amount = self.nominal_amount(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, subsidy_size=subsidy_size, pay_emi_in=pay_emi_in)
+            overall_val = sum(amount['Nominal Amount'])
+        if DCR_status:
+            overall_investment = (project_size * self.DCR) - self.loan_amount(project_size=project_size, subsidy_size=subsidy_size, DCR_status=DCR_status, loan_size=loan_size) - subsidy_size
+        else:
+            overall_investment = (project_size * self.NON_DCR) - self.loan_amount(project_size=project_size, subsidy_size=subsidy_size, DCR_status=DCR_status, loan_size=loan_size) - subsidy_size
+        
+        return (((overall_val/overall_investment)**(1/25))-1)*100
+
+    def _figure_return(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, subsidy_size: float, compare: str, realized: bool, DCR_status: bool):
+        if compare == 'bid_rate':
+            if type(bid_rate) != list:
+                x = np.linspace(0.5*bid_rate, 1.5*bid_rate, 10)
+            else:
+                x = np.linspace(bid_rate[0], bid_rate[-1], 10)
+            #output
+            y = []
+            for val in x:
+                y.append(self._annualized_return(bid_rate=val, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, realized=realized, DCR_status=DCR_status))
+            plt.plot(x, y)
+            plt.scatter(x, y)
+            plt.grid(True)
+            plt.xlabel('Bid Rate')
+            if realized:
+                plt.ylabel(f'Realized Return over {round(self.INFLATION*100, 2)}% inflation')
+            else:
+                plt.ylabel('Nominal Return')
+        elif compare == 'project_size':
+            if type(project_size) != list:
+                x = np.linspace(0.5*project_size, 1.5*project_size, 10)
+            else:
+                x = np.linspace(project_size[0], project_size[-1], 10)
+            #output
+            y = []
+            for val in x:
+                y.append(self._annualized_return(bid_rate=bid_rate, project_size=val, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, realized=realized, DCR_status=DCR_status))
+            plt.plot(x, y)
+            plt.scatter(x, y)
+            plt.grid(True)
+            plt.xlabel('Project Size in MW')
+            if realized:
+                plt.ylabel(f'Realized Return over {round(self.INFLATION*100, 2)}% inflation')
+            else:
+                plt.ylabel('Nominal Return')
+        elif compare == 'loan_size':
+            if type(loan_size) != list:
+                x = np.linspace(0.75*loan_size, 1.25*loan_size, 10)
+            else:
+                x = np.linspace(loan_size[0], loan_size[-1], 10)
+            #output
+            y = []
+            for val in x:
+                y.append(self._annualized_return(bid_rate=bid_rate, project_size=project_size, loan_size=val, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, realized=realized, DCR_status=DCR_status))
+            plt.plot(x, y)
+            plt.scatter(x, y)
+            plt.grid(True)
+            plt.xlabel('Loan Amount')
+            if realized:
+                plt.ylabel(f'Realized Return over {round(self.INFLATION*100, 2)}% inflation')
+            else:
+                plt.ylabel('Nominal Return')
+        elif compare == 'pay_emi_in':
+            if type(pay_emi_in) != list:
+                x = np.arange(int(0.5*pay_emi_in), int(1.5*pay_emi_in), 1)
+            else:
+                x = np.arange(pay_emi_in[0], pay_emi_in[-1], 1)
+            #output
+            y = []
+            for val in x:
+                y.append(self._annualized_return(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=val, subsidy_size=subsidy_size, realized=realized, DCR_status=DCR_status))
+            plt.plot(x, y)
+            plt.scatter(x, y)
+            plt.grid(True)
+            plt.xlabel('EMI Duration')
+            if realized:
+                plt.ylabel(f'Realized Return over {round(self.INFLATION*100, 2)}% inflation')
+            else:
+                plt.ylabel('Nominal Return')
+        elif compare == 'subsidy_size':
+            if type(subsidy_size) != list:
+                x = np.linspace(0.5*subsidy_size, 1.5*subsidy_size, 10)
+            else:
+                x = np.linspace(subsidy_size[0], subsidy_size[-1], 10)
+            #output
+            y = []
+            for val in x:
+                y.append(self._annualized_return(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=val, realized=realized, DCR_status=DCR_status))
+            plt.plot(x, y)
+            plt.scatter(x, y)
+            plt.grid(True)
+            plt.xlabel('Subsidy Size')
+            if realized:
+                plt.ylabel(f'Realized Return over {round(self.INFLATION*100, 2)}% inflation')
+            else:
+                plt.ylabel('Nominal Return')
+        else:
+            print('Error! Get better idiot (Check Spelling),')
+        return
+    
+        return
+    
+    def compare(self, bid_rate: float, project_size: float, loan_size: float, pay_emi_in: int, subsidy_size: float, compare: str, realized=True, DCR_status=True):
+        if compare == 'DCR_status':
+            print('DCR Case')
+            self.annualized_return(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, realized=realized, DCR_status=True)
+            print('-'*10)
+            print('Non-DCR Case')
+            self.annualized_return(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, realized=realized, DCR_status=False)
+        elif compare == 'realized':
+            print('Nominal Return')
+            self.annualized_return(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, realized=False, DCR_status=DCR_status)
+            print('-'*10)
+            print('Realized Return')
+            self.annualized_return(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, realized=True, DCR_status=DCR_status)
+        else:
+            self._figure_return(bid_rate=bid_rate, project_size=project_size, loan_size=loan_size, pay_emi_in=pay_emi_in, subsidy_size=subsidy_size, compare=compare, realized=realized, DCR_status=DCR_status)
         return
